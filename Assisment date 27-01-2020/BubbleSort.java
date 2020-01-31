6902df3a493a7cc479a398d5c641bc69c1c656c6
vasu upadhayay <vasuupadhayaytech@gmail.com>
2020-01-31 16:00:50 +0530

    Add files via upload

---
+++ Assisment date 27-01-2020/BubbleSort.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+
+class BubbleSort{
+
+	
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+		int []value = new int[10];
+
+		int temp;
+
+		System.out.println("please enter numbers");
+
+		for(int i = 0;i<5;i++)
+
+		{
+
+	     value[i] = scan.nextInt();
+
+	    }
+
+	    for(int i = 0;i < 4;i++)
+
+	    {
+
+	    	for(int j = i+1;j < 4;j++)
+
+	    	{
+
+
+
+	    		if(value[j]>value[j+1])
+
+	    		{
+
+	    			temp = value[i];
+
+	    			value[i] = value[j];
+
+	    			value[j] = temp;
+
+
+
+	    		}
+
+	    	}
+
+	    }
+
+	    System.out.println("numbers in sorted order are");
+
+	    for(int i = 0;i < 5;i++)
+
+		{
+
+	     System.out.println(value[i]);
+
+	    }
+
+	}
+
+}
