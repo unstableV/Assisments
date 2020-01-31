e028343dfe830956d13244396d19bf855b1b0ce4
vasu upadhayay <vasuupadhayaytech@gmail.com>
2020-01-31 16:07:56 +0530

    Add files via upload

---
+++ Assisment date 24-01-2020/fibonaci.java
@@ -0,0 +1,21 @@
+class fibonaci{
+    public static void main(String[] args) {
+
+    	int a=0,b=1,show=0,i=2;
+
+        System.out.println("fibonaci series is:");
+    	while (i<100){
+    	if(show==89)
+    	{
+    		break;
+    	}
+        show=a+b;
+        a=b;
+        b=show;
+        ++i;
+        System.out.println(show);    
+    }
+
+}
+
+}
